146e773a0135ac06c4f45aaaa0b4213023a0bc55
Sokol V <1470715@gmail.com>
2018-09-25 19:28:16 +0300

    fast exchange

--- src/index.js
+++ src/index.js
@@ -1,5 +1,25 @@
 // PLEASE DON'T change function name
 module.exports = function makeExchange(currency) {
-    // Your code goes here!
-    // Return an object containing the minimum number of coins needed to make change
+  if(currency <= 0){
+    return {};
+  } else if(currency <= 10000){
+    let monetDenomination = [50, 25, 10, 5, 1];
+    let monetName = ["H", "Q", "D", "N", "P"];
+    let result = {};
+
+    for(let i = 0, len = monetDenomination.length; i < len; i++){
+      if(currency >= monetDenomination[i]){
+        let count = 0;
+        while(count * monetDenomination[i] <= currency - monetDenomination[i]){
+            count++;
+        }
+        currency -= monetDenomination[i] * count;
+        result[monetName[i]] = count;
+      }
+    }
+
+    return result;
+  } else {
+    return {error: "You are rich, my friend! We don't have so much coins for exchange"};
+  }
 }
